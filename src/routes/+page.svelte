17e094429956c4bb9196d717c13b32adf47ee84b
Eunjae Lee <hey@eunjae.dev>
2023-03-07 21:23:21 +0100

    chore: add supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import type { Session } from '@supabase/supabase-js';
+
+	export let data: PageData;
+
+	let { supabase, session } = data;
+
+	$: if (data.session) {
+		console.log('💡 try to load data');
+		// loadData();
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
