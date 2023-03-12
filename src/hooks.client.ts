131380eb90921ffb6020dada381acc15fc81fd1b
Eunjae Lee <hey@eunjae.dev>
2023-03-12 17:29:51 +0100

    track with sentry only on production mode

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,16 +2,20 @@ import * as SentrySvelte from '@sentry/svelte';
 import { BrowserTracing } from '@sentry/tracing';
 import type { HandleClientError } from '@sveltejs/kit';
 
-SentrySvelte.init({
-	dsn: 'https://edcddf8ea1b74ea89350942bdb0c3bb8@o401874.ingest.sentry.io/4504826500218880',
-	integrations: [new BrowserTracing()],
-	tracesSampleRate: 1.0
-});
+if (process.env.NODE_ENV === 'production') {
+	SentrySvelte.init({
+		dsn: 'https://edcddf8ea1b74ea89350942bdb0c3bb8@o401874.ingest.sentry.io/4504826500218880',
+		integrations: [new BrowserTracing()],
+		tracesSampleRate: 1.0
+	});
 
-SentrySvelte.setTag('svelteKit', 'browser');
+	SentrySvelte.setTag('svelteKit', 'browser');
+}
 
 export const handleError = (({ error, event }) => {
-	SentrySvelte.captureException(error, { contexts: { sveltekit: { event } } });
+	if (process.env.NODE_ENV === 'production') {
+		SentrySvelte.captureException(error, { contexts: { sveltekit: { event } } });
+	}
 
 	return {
 		message: (error as Error).message
