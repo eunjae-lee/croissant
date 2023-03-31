3571b37e53f593b3bfda23408931ab68de55b76d
Eunjae Lee <hey@eunjae.dev>
2023-03-31 23:11:23 +0200

    update style

--- src/routes/decks/[slug]/add-json/+page.svelte
+++ src/routes/decks/[slug]/add-json/+page.svelte
@@ -8,6 +8,8 @@
 	import Container from '$lib/components/Container.svelte';
 	import { onDestroy, onMount } from 'svelte';
 	import Prompt from '$lib/components/Prompt.svelte';
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+	import { Info } from 'lucide-svelte';
 
 	export let data: PageData;
 
@@ -97,11 +99,12 @@
 
 <div class="mt-8">
 	<Container>
-		<details>
-			<summary>Basic Instruction</summary>
-			<div>
-				<p>Enter a JSON string containing an array of items with `front` and `back` keys.</p>
-				<code>
+		<Accordion>
+			<AccordionItem>
+				<svelte:fragment slot="lead"><Info /></svelte:fragment>
+				<svelte:fragment slot="summary">Basic Instruction</svelte:fragment>
+				<svelte:fragment slot="content">
+					<p>Enter a JSON string containing an array of items with `front` and `back` keys.</p>
 					<pre class="mt-2">{@html JSON.stringify(
 							[
 								{ front: 'Hello', back: 'Bonjour' },
@@ -110,43 +113,43 @@
 							null,
 							2
 						)}</pre>
-				</code>
-			</div>
-		</details>
+				</svelte:fragment>
+			</AccordionItem>
+			<AccordionItem>
+				<svelte:fragment slot="lead"><Info /></svelte:fragment>
+				<svelte:fragment slot="summary">Using Generative AI (e.g. ChatGPT)</svelte:fragment>
+				<svelte:fragment slot="content"
+					><p>
+						If you're using generative AI like <a
+							href="https://chat.openai.com/"
+							target="_blank"
+							rel="noopener noreferrer">ChatGPT</a
+						>, you can ask it to generate JSON for you. The example prompts are like the following.
+					</p>
+					<div class="flex flex-col gap-4">
+						<Prompt texts={[`Write me 5 sentences I can use in a café at an A2 level.`]} />
+						<Prompt
+							texts={[
+								`Pick 3 basic verbs, and give me total 24 examples by mixing different subjects with those verbs in present tense.`
+							]}
+						/>
+						<Prompt
+							texts={[
+								`I'm about to call a real-estate agency to look for an apartment. Give me 5 basic sentences that I could use.`
+							]}
+						/>
+					</div></svelte:fragment
+				>
+			</AccordionItem>
+		</Accordion>
 
-		<details>
-			<summary>Using Generative AI (e.g. ChatGPT)</summary>
-			<div>
-				<p>
-					If you're using generative AI like <a
-						href="https://chat.openai.com/"
-						target="_blank"
-						rel="noopener noreferrer">ChatGPT</a
-					>, you can ask it to generate JSON for you. The example prompts are like the following.
-				</p>
-				<div class="flex flex-col gap-4">
-					<Prompt texts={[`Write me 5 sentences I can use in a café at an A2 level.`]} />
-					<Prompt
-						texts={[
-							`Pick 3 basic verbs, and give me total 24 examples by mixing different subjects with those verbs in present tense.`
-						]}
-					/>
-					<Prompt
-						texts={[
-							`I'm about to call a real-estate agency to look for an apartment. Give me 5 basic sentences that I could use.`
-						]}
-					/>
-				</div>
-			</div>
-		</details>
-
-		<form class="card w-full bg-base-100 shadow-xl" on:submit|preventDefault={onSubmit}>
+		<form class="mt-8 card" on:submit|preventDefault={onSubmit}>
 			<div class="card-body p-4 gap-4 sm:p-8 sm:gap-8">
 				<div bind:this={monacoElem} class="w-full h-96" />
 				<div class="card-actions justify-center">
 					<button
 						type="submit"
-						class="btn btn-primary w-full text-lg py-4 sm:text-2xl sm:py-8 h-[initial]"
+						class="mt-4 btn btn-lg sm:btn-xl variant-soft-primary w-full"
 						class:loading={status === 'submitting'}
 						disabled={status === 'submitting'}>Add JSON</button
 					>
@@ -154,8 +157,8 @@
 			</div>
 		</form>
 		<div class="mt-4 flex justify-between">
-			<a href="./add" class="btn btn-ghost opacity-50 hover:opacity-100">Add card</a>
-			<a href="./play" class="btn btn-ghost opacity-50 hover:opacity-100">Play cards ?</a>
+			<a href="./add" class="btn variant-soft">Add card</a>
+			<a href="./play" class="btn variant-soft">Play cards ?</a>
 		</div>
 	</Container>
 
@@ -179,20 +182,3 @@
 		</div>
 	{/if}
 </div>
-
-<style>
-	details {
-		@apply mb-4;
-	}
-	summary {
-		@apply ml-2 cursor-pointer;
-	}
-
-	summary + div {
-		@apply ml-2;
-	}
-
-	pre {
-		@apply border border-base-content rounded-lg py-2 px-4;
-	}
-</style>
