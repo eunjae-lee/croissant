e5db51c6f2c3f8de4cd9a6bef948796559ab1433
Eunjae Lee <hey@eunjae.dev>
2023-06-20 20:07:45 +0200

    adding TextComparison WIP

---
+++ src/lib/components/TextComparison.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import diff from 'simple-text-diff';
+
+	export let correctAnswer: string;
+	export let yourAnswer: string;
+
+	let result = diff.diffPatch(correctAnswer, yourAnswer);
+</script>
+
+correct
+<div class="correct-answer">{@html result.before}</div>
+
+your answer
+<div class="your-answer">{@html result.after}</div>
+
+<style>
+	.correct-answer :global(del) {
+		@apply no-underline bg-green-300;
+	}
+
+	.your-answer :global(ins) {
+		@apply line-through bg-red-300;
+	}
+</style>
