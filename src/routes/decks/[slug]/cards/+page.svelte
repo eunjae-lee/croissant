9d953bcbf6450008c90e3469d785e1b37a8f2ee4
Eunjae Lee <hey@eunjae.dev>
2023-07-01 16:21:19 +0200

    fix card view

--- src/routes/decks/[slug]/cards/+page.svelte
+++ src/routes/decks/[slug]/cards/+page.svelte
@@ -3,16 +3,19 @@
 	import NavBar from '$lib/components/NavBar.svelte';
 	import type { PageData } from './$types';
 	import Container from '$lib/components/Container.svelte';
-	import { ArrowLeft, ArrowRight, Undo2 } from 'lucide-svelte';
+	import { ArrowLeft, ArrowRight, Home, Undo2 } from 'lucide-svelte';
 	import { AppShell } from '@skeletonlabs/skeleton';
 	import { goto } from '$app/navigation';
+	import { splitStringWithCloze } from '$lib/utils';
 
 	export let data: PageData;
 
 	let currentIndex = 0;
 
 	$: currentCard = data.cards[currentIndex];
-	$: currentContent = showingBack ? currentCard.back : currentCard.front;
+	$: linesSplitWithCloze = (showingBack ? currentCard.back : currentCard.front)
+		.split('\n')
+		.map(splitStringWithCloze);
 	$: totalNumberOfCards = data.cards.length;
 
 	let showingBack = true;
@@ -35,40 +38,42 @@
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<NavBar deck={data.deck} />
+		<Container>
+			<div class="mt-2 flex items-center">
+				<div class="grow basis-1/3 flex gap-1">
+					<button type="button" on:click={() => move(-1)} class="btn btn-sm variant-soft"
+						><ArrowLeft /></button
+					>
+					<a href="/decks" class="btn btn-sm variant-soft"><Home /></a>
+				</div>
+				<div class="badge">{`${currentIndex + 1}/${totalNumberOfCards}`}</div>
+				<div class="grow basis-1/3 flex gap-1 justify-end">
+					<button type="button" on:click={flip} class="btn btn-sm variant-soft"><Undo2 /></button>
+					<button type="button" on:click={() => move(1)} class="btn btn-sm variant-soft"
+						><ArrowRight /></button
+					>
+				</div>
+			</div>
+		</Container>
 	</svelte:fragment>
 
-	<Container>
-		<div class="mt-1 flex items-center">
-			<button type="button" on:click={() => move(-1)} class="btn btn-sm variant-soft"
-				><ArrowLeft /></button
-			>
-			<button type="button" on:click={flip} class="ml-1 btn btn-sm variant-soft"><Undo2 /></button>
-			<div class="grow" />
-			<div class="badge">{`${currentIndex + 1}/${totalNumberOfCards}`}</div>
-			<div class="grow" />
-			<button type="button" on:click={() => move(1)} class="btn btn-sm variant-soft"
-				><ArrowRight /></button
-			>
-		</div>
-	</Container>
 	{#key currentCard.id}
-		<div class="mt-4 px-4 sm:px-8">
-			<div
-				class="w-full almost-full-height sm:w-[32rem] md:w-[48rem] flex items-center justify-center"
-			>
-				<div class="mt-2 p-4">
-					{#each currentContent.replaceAll('{{', '').replaceAll('}}', '').split('\n') as line}
-						<p class="text-3xl">{line}</p>
-					{/each}
-				</div>
+		<div class="m-4 sm:m-8">
+			<div class="space-y-4">
+				{#each linesSplitWithCloze as line}
+					<p class="text-3xl">
+						{#each line as item, index (index)}
+							{#if item.type === 'text'}
+								<span>{@html item.content.split('\n').join('<br />')}</span>
+							{:else if item.type === 'cloze'}
+								<span class="text-primary-600 font-bold"
+									>{@html item.content.split('\n').join('<br />')}</span
+								>
+							{/if}
+						{/each}
+					</p>
+				{/each}
 			</div>
 		</div>
 	{/key}
 </AppShell>
-
-<style>
-	.almost-full-height {
-		height: calc(100vh - 12rem);
-	}
-</style>
